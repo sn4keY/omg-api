cc0625e746cf31989ec8d020bdc53eb2d432e5f8
Norbert Neudert <norbertneudert@gmail.com>
2020-09-25 14:09:59 +0200

    Add StoredPlate entity

---
+++ src/OpenMyGarage.Entity/Entity/StoredPlate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OpenMyGarage.Entity.Entity
+{
+    public class StoredPlate
+    {
+        [Key]
+        [Required]
+        public string Plate { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Nationality { get; set; }
+
+        [Required]
+        public string CarManufacturer { get; set; }
+
+        public bool AutoOpen { get; set; }
+    }
+}
