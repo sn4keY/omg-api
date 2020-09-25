8f6f9213c6a336d5bfb49be1a80e8c93a61088e7
Norbert Neudert <norbertneudert@gmail.com>
2020-09-25 15:43:21 +0200

    Fixed parameters in Get function

--- src/OpenMyGarage.Entity/Repository/IRepository.cs
+++ src/OpenMyGarage.Entity/Repository/IRepository.cs
@@ -11,8 +11,7 @@ namespace OpenMyGarage.Entity.Repository
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> Get(
             Expression<Func<TEntity, bool>> filter = null, 
-            Func<IQueryable<TEntity>> orderBy = null, 
-            string includeProperties = "");
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null);
         TEntity GetByID(object id);
         void Insert(TEntity entity);
         void Delete(object id);
