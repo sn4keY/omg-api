705def97999e86e207174020895c01f4d85684f6
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 11:14:11 +0200

    Add IUnitOfWork interface

---
+++ src/OpenMyGarage.Entity/UnitofWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using OpenMyGarage.Entity.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenMyGarage.Entity.UnitofWork
+{
+    interface IUnitOfWork
+    {
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+        void Save();
+    }
+}
