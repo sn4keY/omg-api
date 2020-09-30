ef8ccba220c7b25c36ae2d54f02dff506b7398c8
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 11:59:53 +0200

    Add LoginViewModel

---
+++ src/OpenMyGarage.Domain/ViewModel/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenMyGarage.Domain.ViewModel
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
