b5f46d4e48cf4e9858a76272781a82cc8eddcbbb
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 15:44:17 +0200

    Add migration userprivileges

--- src/OpenMyGarage.Entity/Migrations/20200930134345_userprivileges.Designer.cs
+++ src/OpenMyGarage.Entity/Migrations/20200930134345_userprivileges.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpenMyGarage.Entity.Data;
 
 namespace OpenMyGarage.Entity.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200930134345_userprivileges")]
+    partial class userprivileges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
