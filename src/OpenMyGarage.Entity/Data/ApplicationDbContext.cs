066b42faad3abe99b7ea08778a63d88fe4c148cc
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 15:41:19 +0200

    Added Many-to-many relationship

--- src/OpenMyGarage.Entity/Data/ApplicationDbContext.cs
+++ src/OpenMyGarage.Entity/Data/ApplicationDbContext.cs
@@ -15,5 +15,20 @@ namespace OpenMyGarage.Entity.Data
             : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Entity.UserPrivilege>().HasKey(up => new { up.UserId, up.PrivilegeId });
+            builder.Entity<Entity.UserPrivilege>()
+                .HasOne(up => up.ApplicationUser)
+                .WithMany(au => au.Privileges)
+                .HasForeignKey(p => p.UserId);
+            builder.Entity<Entity.UserPrivilege>()
+                .HasOne(up => up.Privilege)
+                .WithMany()
+                .HasForeignKey(p => p.PrivilegeId);
+        }
     }
 }
