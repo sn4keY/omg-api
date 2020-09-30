948d75c2662bb100812da0103fe81a43aab17472
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 15:28:21 +0200

    Add ApplicationUser class

---
+++ src/OpenMyGarage.Entity/Entity/UserPrivileges/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenMyGarage.Entity.Entity.UserPrivileges
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ICollection<Privilege> Privileges { get; set; }
+    }
+}
