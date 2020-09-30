3792b7cf448b1fc0b95b7d8369aeb94d019fb079
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 11:55:04 +0200

    Add StoredPlateViewModel

---
+++ src/OpenMyGarage.Domain/ViewModel/StoredPlateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenMyGarage.Domain.ViewModel
+{
+    public class StoredPlateViewModel
+    {
+        public string Plate { get; set; }
+        public string Name { get; set; }
+        public string Nationality { get; set; }
+        public string CarManufacturer { get; set; }
+        public bool AutoOpen { get; set; }
+    }
+}
