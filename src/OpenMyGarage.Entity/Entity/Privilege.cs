99c8e16283d97c41b2261fe80718f90795f30c74
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 15:25:26 +0200

    Add Privilege entity

---
+++ src/OpenMyGarage.Entity/Entity/Privilege.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OpenMyGarage.Entity.Entity.UserPrivileges
+{
+    public class Privilege
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public UserPrivilege UserPrivilege { get; set; }
+    }
+}
