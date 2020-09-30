b2581180c7863061c75d00b15e594516c9bbd5d3
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 11:53:00 +0200

    Add EntryLogViewModel

---
+++ src/OpenMyGarage.Domain/ViewModel/EntryLogViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenMyGarage.Domain.ViewModel
+{
+    public class EntryLogViewModel
+    {
+        public string Plate { get; set; }
+        public long EntryTime { get; set; }
+        public string PictureURL { get; set; }
+    }
+}
