a8bf53a45633caada0110d6a02f0af7f181a516e
Norbert Neudert <norbertneudert@gmail.com>
2020-09-30 12:22:56 +0200

    Add MappingProfile

---
+++ src/OpenMyGarage.Api/Mapper/MappigProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using OpenMyGarage.Domain.ViewModel;
+using OpenMyGarage.Entity.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenMyGarage.Api.Mapper
+{
+    public class MappigProfile : Profile
+    {
+        public MappigProfile()
+        {
+            CreateMap<EntryLogViewModel, EntryLog>();
+            CreateMap<EntryLog, EntryLogViewModel>();
+
+            CreateMap<StoredPlateViewModel, StoredPlate>();
+            CreateMap<StoredPlate, StoredPlateViewModel>();
+        }
+    }
+}
