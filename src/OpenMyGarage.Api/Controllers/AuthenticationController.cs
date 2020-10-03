6623686747a1cdb95c878da38808ded6901487ae
Norbert Neudert <norbertneudert@gmail.com>
2020-10-03 17:00:55 +0200

    Add AuthenticationController

---
+++ src/OpenMyGarage.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenMyGarage.Domain.Service;
+using OpenMyGarage.Domain.ViewModel;
+
+namespace OpenMyGarage.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationServiceAsync authenticationService;
+
+        public AuthenticationController(IAuthenticationServiceAsync service)
+        {
+            this.authenticationService = service;
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Register([FromBody] RegisterViewModel newUser)
+        {
+            return await authenticationService.RegisterUser(newUser);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login([FromBody] LoginViewModel user)
+        {
+            return await authenticationService.LoginUser(user);
+        }
+    }
+}
