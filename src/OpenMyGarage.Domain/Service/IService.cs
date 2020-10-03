c797853f744aed68b6f5c52be641a66a277c1bdd
Norbert Neudert <norbertneudert@gmail.com>
2020-10-03 13:36:11 +0200

    Add IService interface

---
+++ src/OpenMyGarage.Domain/Service/IService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OpenMyGarage.Domain.Service
+{
+    public interface IService<TV, TE>
+    {
+        IEnumerable<TV> GetAll();
+        IEnumerable<TV> Get(
+            Expression<Func<TE, bool>> filter = null,
+            Func<IQueryable<TE>, IOrderedQueryable<TE>> orderBy = null);
+        TV GetById(object id);
+        void Insert(TV vm);
+        void Delete(object id);
+        void Delete(TV vm);
+        void Update(TV vmToUpdate);
+    }
+}
