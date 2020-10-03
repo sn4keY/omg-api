c5b82626e2d94e6c44612d01a5da69abfb955603
Norbert Neudert <norbertneudert@gmail.com>
2020-10-03 15:15:43 +0200

    Add IAthenticationServiceAsync interface

---
+++ src/OpenMyGarage.Domain/Service/IAuthenticationServiceAsync.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenMyGarage.Domain.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMyGarage.Domain.Service
+{
+    public interface IAuthenticationServiceAsync
+    {
+        Task<ActionResult> RegisterUser(RegisterViewModel vm);
+        Task<ActionResult> LoginUser(LoginViewModel vm);
+    }
+}
