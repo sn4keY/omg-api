042d0686662e872b945a8174649dd3483577a456
Norbert Neudert <norbertneudert@gmail.com>
2020-10-03 16:21:09 +0200

    Add EntryLogController

---
+++ src/OpenMyGarage.Api/Controllers/EntryLogController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenMyGarage.Domain.Service;
+using OpenMyGarage.Domain.ViewModel;
+using OpenMyGarage.Entity.Entity;
+
+namespace OpenMyGarage.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EntryLogController : ControllerBase
+    {
+        private readonly IService<EntryLogViewModel, EntryLog> entryLogService;
+
+        public EntryLogController(IService<EntryLogViewModel, EntryLog> service)
+        {
+            this.entryLogService = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<EntryLogViewModel>> GetEntryLogs()
+        {
+            var entryLogs = entryLogService.GetAll();
+
+            return entryLogs.ToList();
+        }
+    }
+}
