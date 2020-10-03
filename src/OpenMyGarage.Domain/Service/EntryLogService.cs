310ff7c3c91be12e79f1202abc648638c86e2d9f
Norbert Neudert <norbertneudert@gmail.com>
2020-10-03 14:05:54 +0200

    Created EntryLogService

---
+++ src/OpenMyGarage.Domain/Service/EntryLogService.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions.Impl;
+using OpenMyGarage.Domain.ViewModel;
+using OpenMyGarage.Entity.Entity;
+using OpenMyGarage.Entity.UnitofWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OpenMyGarage.Domain.Service
+{
+    public class EntryLogService : IService<EntryLogViewModel, EntryLog> 
+    {
+        private IUnitOfWork unitOfWork;
+        private IMapper mapper;
+
+        public EntryLogService(IUnitOfWork uof, IMapper m)
+        {
+            this.unitOfWork = uof;
+            this.mapper = m;
+        }
+
+        public IEnumerable<EntryLogViewModel> GetAll()
+        {
+            var entities = unitOfWork.GetRepository<EntryLog>().GetAll();
+
+            return mapper.Map<IEnumerable<EntryLogViewModel>>(entities);
+        }
+
+        public IEnumerable<EntryLogViewModel> Get(Expression<Func<EntryLog, bool>> filter = null, Func<IQueryable<EntryLog>, IOrderedQueryable<EntryLog>> orderBy = null)
+        {
+            var entities = unitOfWork.GetRepository<EntryLog>().Get(filter, orderBy);
+
+            return mapper.Map<IEnumerable<EntryLogViewModel>>(entities);
+        }
+
+        public EntryLogViewModel GetById(object id)
+        {
+            var entity = unitOfWork.GetRepository<EntryLog>().GetByID(id);
+
+            return mapper.Map<EntryLogViewModel>(entity);
+        }
+
+        public void Insert(EntryLogViewModel vm)
+        {
+            var entity = mapper.Map<EntryLog>(vm);
+            unitOfWork.GetRepository<EntryLog>().Insert(entity);
+            unitOfWork.Save();
+        }
+
+        public void Delete(object id)
+        {
+            unitOfWork.GetRepository<EntryLog>().Delete(id);
+            unitOfWork.Save();
+        }
+
+        public void Delete(EntryLogViewModel vm)
+        {
+            var entity = mapper.Map<EntryLog>(vm);
+            unitOfWork.GetRepository<EntryLog>().Delete(entity);
+            unitOfWork.Save();
+        }
+
+        public void Update(EntryLogViewModel vmToUpdate)
+        {
+            var entity = mapper.Map<EntryLog>(vmToUpdate);
+            unitOfWork.GetRepository<EntryLog>().Update(entity);
+        }
+    }
+}
